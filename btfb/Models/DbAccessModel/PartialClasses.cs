bc347026e58c6f482ad1bf3467eb6b00c5cbb2e6
Omar Fernandez <ofernandez@orizon360.com>
2018-05-06 12:57:36 -0400

    partial class method to send empty make and model when value in reques is null also webgrid working still have to create helper but that is for version 2

--- btfb/Models/DbAccessModel/PartialClasses.cs
+++ btfb/Models/DbAccessModel/PartialClasses.cs
@@ -8,6 +8,28 @@ namespace btfb.Models.DbAccessModel
     [MetadataType(typeof(RequestsMetadata))]
     public partial class Request
     {
+        public string MakeName
+        {
+            get
+            {
+                var makename = string.Empty;
+                if (this.Make1 != null && this.Make1.Make1 != null)
+                    makename = this.Make1.Make1;
 
+                return makename;
+            }
+        }
+        public string ModelName
+        {
+            get
+            {
+                var modelname = string.Empty;
+                if(this.Model1!=null && this.Model1.Model1 != null)
+                {
+                    modelname = this.Model1.Model1;
+                }
+                return modelname;
+            }
+        }
     }
 }
